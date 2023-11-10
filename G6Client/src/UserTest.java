5ba15366ac0b9b90b0959a59481a9c7ef3949fda
oussamaekko <oussamabouhamidi2000@gmail.com>
2023-11-10 23:49:56 +0100

    first commit

---
+++ G6Client/src/UserTest.java
@@ -0,0 +1,42 @@
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import dao.IDao;
+import entities.User;
+
+public class UserTest {
+	
+	public static IDao<User> lookUpEmployeRemote() throws NamingException {
+		final Hashtable jndiProperties = new Hashtable();
+		jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
+		jndiProperties.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
+		final Context context = new InitialContext(jndiProperties);
+
+		return (IDao<User>) context.lookup("ejb:G6EAR/G6Serveur/userS!dao.IDao");
+										
+	}
+	public static void main(String[] args) {
+		try {
+			IDao<User> dao = lookUpEmployeRemote();
+			/*dao.create(new User("RAMI", "Samir"));
+			dao.create(new User("SAFI", "Imane"));
+			dao.create(new User("ALAMI", "Kamal"));*/
+			dao.findAll();
+			
+			for(User e : dao.findAll())
+				{
+					System.out.println("Login : " + e.getLogin());
+					System.out.print("Password" + e.getPassword());
+				}
+				
+			
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ G6WebClient/src/main/java/controller/UserController.java
@@ -0,0 +1,48 @@
+package controller;
+
+import jakarta.ejb.EJB;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import dao.IDaoLocal;
+import entities.User;
+
+/**
+ * Servlet implementation class UserController
+ */
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@EJB
+	private IDaoLocal<User> dao;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		dao.create(new User("pass","log"));
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
